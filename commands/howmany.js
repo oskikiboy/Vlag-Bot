1cb1b0e04881c4630a524d169a48a6dc9530e849
Frangu Vlad <kingdgrizzle@gmail.com>
2017-07-24 16:08:00 +0300

    aesthethics

--- commands/howmany.js
+++ commands/howmany.js
@@ -1,16 +1,16 @@
 exports.run = async (bot, msg, suffix) => {
 	if (suffix.trim().toLowerCase() === "commands" || suffix.trim().toLowerCase() === "cmds") {
-		return msg.reply({
+		return msg.channel.send({
 			embed: {
 				color: 0x3669FA,
-				description: `There are ${Object.keys(require("../commands.js")).length} commands.`,
+				description: `There are \`${Object.keys(require("../commands.js")).length}\` commands.`,
 			},
 		});
 	} else if (suffix.trim().toLowerCase() === "usage") {
-		return msg.reply({
+		return msg.channel.send({
 			embed: {
 				color: 0x3669FA,
-				description: `${require("../bot.js").usage} commands were run so far from the last reboot.`,
+				description: `**${require("../bot.js").usage}** commands were run so far from the last reboot.`,
 			},
 		});
 	}
