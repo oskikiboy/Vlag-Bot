1513c7a4567dbf4383d204b685ed685abd35fef9
Frangu Vlad <kingdgrizzle@gmail.com>
2017-07-25 11:10:16 +0300

    Frogot how embed objects work..

--- commands/sorarin.js
+++ commands/sorarin.js
@@ -5,10 +5,10 @@ exports.run = async (bot, msg, suffix) => {
 			image: {
 				url: `https://s20.postimg.org/rjfszt2pp/Sorarin_meme_1.png`,
 			},
-			description: `<@212748677041618944> likes giving that tongue. 👅`
-		},
-		footer: {
-			text: `A quote from Sorarin herself: "I eat pussy xd"`,
+			description: `<@212748677041618944> likes giving that tongue. 👅`,
+			footer: {
+				text: `A quote from Sorarin herself: "I eat pussy xd"`,
+			},
 		},
 	});
 };
