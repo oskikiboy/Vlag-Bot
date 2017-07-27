bad0cb719a5dbfd67fddc2b67eb064c8563e0353
Frangu Vlad <kingdgrizzle@gmail.com>
2017-07-27 11:05:50 +0300

    kthx postimg

--- commands/balls.js
+++ commands/balls.js
@@ -2,7 +2,7 @@ exports.run = async (bot, msg, suffix) => {
 	msg.channel.send({
 		embed: {
 			image: {
-				url: `https://s20.postimg.org/el5k78q9p/haden_wtf_1.png`,
+				url: `http://s20.postimg.org/el5k78q9p/haden_wtf_1.png`,
 			},
 			color: 0x3669FA,
 			description: `Oops, looks like <@144260472575754241> dropped his balls. ¯\\_(ツ)_/¯`,
