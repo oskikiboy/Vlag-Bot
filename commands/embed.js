3970209635ad597d3d22e8f18d7544b47917dcba
Frangu Vlad <kingdgrizzle@gmail.com>
2017-07-27 11:01:04 +0300

    yeahyeah

--- commands/embed.js
+++ commands/embed.js
@@ -1,14 +1,23 @@
 exports.run = async (bot, msg, suffix) => {
-	msg.channel.send({
-		embed: {
-			color: 0x3669FA,
-			author: {
-				name: `${msg.author.tag} requested an embed`,
-				icon_url: msg.author.displayAvatarURL,
+	if (suffix) {
+		msg.channel.send({
+			embed: {
+				color: 0x3669FA,
+				author: {
+					name: `${msg.author.tag} requested an embed`,
+					icon_url: msg.author.displayAvatarURL,
+				},
+				image: {
+					url: suffix.trim(),
+				},
 			},
-			image: {
-				url: suffix.trim(),
+		});
+	} else {
+		msg.channel.send({
+			embed: {
+				color: 0xFF0000,
+				description: `What do you want to embed? Preferably an image :smile:`,
 			},
-		},
-	});
+		});
+	}
 };
