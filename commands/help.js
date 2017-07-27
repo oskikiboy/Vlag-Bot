14defac97296a4349a6b06ea08a3a38c4e258e04
Frangu Vlad <kingdgrizzle@gmail.com>
2017-07-27 10:57:19 +0300

    10 cmds per embed pls kthx

--- commands/help.js
+++ commands/help.js
@@ -36,7 +36,7 @@ exports.run = async (bot, msg, suffix) => {
 			output.push(template(command, commands[command].aliases, commands[command].usage, commands[command].maintainer));
 		}
 		if (output.join("\n").length > 1980) {
-			const split = output.chunk(8);
+			const split = output.chunk(10);
 			for (const splitted of split) {
 				msg.author.send({
 					embed: {
