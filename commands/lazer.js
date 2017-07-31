fa20f08bd676df483a8bc177a839c9af420632bd
laz3rpenguin <w.tisdall@gmail.com>
2017-07-31 18:51:48 +1200

    Create lazer.js

---
+++ commands/lazer.js
@@ -0,0 +1,10 @@
+
+exports.run = async (bot, msg, suffix) => {
+	msg.channel.send({
+		embed: {
+			color: 0x002e59,
+			description: `V-BOT IS MY COPYRIGHT!!!`,
+      image: https://lazer.cf/assets/lazerbot/icon.png`,
+		},
+	});
+};
