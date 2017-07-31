c6ff81191725e186cb089e357e091141a1b629f7
Frangu Vlad <contact@kingdgrizzle.ml>
2017-07-31 09:58:00 +0300

    Update lazer.js

--- commands/lazer.js
+++ commands/lazer.js
@@ -4,7 +4,9 @@ exports.run = async (bot, msg, suffix) => {
 		embed: {
 			color: 0x002e59,
 			description: `V-BOT IS MY COPYRIGHT!!!`,
-      image: https://lazer.cf/assets/lazerbot/icon.png`,
+      			image: {
+				url: `https://lazer.cf/assets/lazerbot/icon.png`,
+			},
 		},
 	});
 };
