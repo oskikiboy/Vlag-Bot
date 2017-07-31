641488b7cd03fb61259ecd4b2e65581379345143
Frangu Vlad <kingdgrizzle@gmail.com>
2017-07-31 09:45:50 +0300

    remove pic from here too

--- commands/haden.js
+++ commands/haden.js
@@ -2,7 +2,6 @@ exports.run = async (bot, msg, suffix) => {
 	let array = [
 		"https://s20.postimg.org/9wkyybbp9/haden_why_AAA_1.png",
 		"https://s20.postimg.org/3kaf27y0t/haden_bulge_1.png",
-		"http://s20.postimg.org/el5k78q9p/haden_wtf_1.png",
 	];
 	msg.channel.send({
 		embed: {
