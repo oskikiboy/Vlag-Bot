1dfa8285f00f702fa1c26002730f3296f95903de
Will (Lazer) <laz3erpenguin@gmail.com>
2017-08-15 15:48:49 +1200

    Remove author tag.
    Sorry copied this from my bot.

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -1,11 +1,6 @@
 exports.run = async (bot, msg, suffix) => {
 	msg.channel.send({
 		embed: {
-			author: {
-                    name: `${bot.user.username}`,
-                    icon_url: `${bot.user.avatarURL}`,
-                    url: "https://github.com/laz3rpenguin/LazerBot"
-			},
 			color: 0x3669FA,
 			description: `It's mining day bro with that water bucket flow. 500,000 diamonds man; never done before. Passing all the competition DanTDM is next.`,
 			thumbnail: {
