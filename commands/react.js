37f6b745cda258f28bf9071ef69ab7d3bb7e8e0d
Frangu Vlad <contact@kingdgrizzle.ml>
2017-08-30 22:17:39 +0300

    help me

--- commands/react.js
+++ commands/react.js
@@ -1,11 +1,14 @@
 exports.run = async (bot, msg, suffix) => {
-	if (!msg.member.roles.get("259858538308960257") || !msg.member.roles.get("259847832121507843")) return msg.reply(`you don't have permission to run this command`).then(m => m.delete(10 * 100));
-	if (!new RegExp(/\d/).test(suffix)) return msg.reply(`thats not a valid message ID.`).then(m => m.delete(10 * 100));
-	try {
-		let fetched = msg.channel.fetchMessage(suffix.trim());
-		await fetched.react(`👍`);
-		await fetched.react(`👎`);
-	} catch (err) {
-		msg.reply(`an error occured!\n${err.message}`);
+	if (msg.member.roles.has("259858538308960257") || msg.member.roles.has("259847832121507843")) {
+		if (!new RegExp(/\d/).test(suffix)) return msg.reply(`thats not a valid message ID.`).then(m => m.delete(10 * 100));
+		try {
+			let fetched = msg.channel.fetchMessage(suffix.trim());
+			await fetched.react(`👍`);
+			await fetched.react(`👎`);
+		} catch (err) {
+			msg.reply(`an error occured!\n${err.message}`);
+		}
+	} else {
+		return msg.reply(`you don't have permission to run this command`).then(m => m.delete(10 * 100));
 	}
 };
