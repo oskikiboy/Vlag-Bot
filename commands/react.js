62d8a161e106e5a16fbc34ca8eb3b8b4c27bbeb7
Frangu Vlad <contact@kingdgrizzle.ml>
2017-08-30 22:25:22 +0300

    am dun

--- commands/react.js
+++ commands/react.js
@@ -2,9 +2,10 @@ exports.run = async (bot, msg, suffix) => {
 	if (msg.member.roles.has("259858538308960257") || msg.member.roles.has("259847832121507843")) {
 		if (!new RegExp(/\d/).test(suffix)) return msg.reply(`thats not a valid message ID.`).then(m => m.delete(15 * 1000));
 		try {
-			let fetched = msg.channel.fetchMessage(suffix.trim());
+			let fetched = await msg.channel.fetchMessage(suffix.trim());
 			await fetched.react(`👍`);
 			await fetched.react(`👎`);
+			await msg.delete();
 		} catch (err) {
 			msg.reply(`an error occured!\n${err.message}`);
 		}
