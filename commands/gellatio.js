d2b8f7a6513dfacdeddb22ce4e1c6f126e3c49af
Frangu Vlad <contact@kingdgrizzle.ml>
2017-09-15 17:40:55 +0300

    Update gellatio.js

--- commands/gellatio.js
+++ commands/gellatio.js
@@ -3,9 +3,9 @@ exports.run = async (bot, msg, suffix) => {
 		"http://sweets.seriouseats.com/images/2015/04/20150424-larte-del-gelato-robyn-lee-redo.jpg",
 		"https://static1.squarespace.com/static/56ba200227d4bdb5579a1157/t/56ba2683f850828a66da240e/1455115880668/dojo+gelato+ice+cream+scoops+home+page.jpeg",
 		"http://www.gelatogo.net/wp-content/uploads/2014/11/big_gelato.png",
-    "http://lartedelgelato.com/wp/wp-content/uploads/2016/01/lartedelgelato-3.jpg",
-    "http://www.gelatogo.net/wp-content/uploads/2016/09/gelato-organico.jpg",
-    "http://img.aws.livestrongcdn.com/ls-article-image-673/cpi.studiod.com/www_livestrong_com/photos.demandstudios.com/getty/article/103/35/483914165_XS.jpg",
+    		"http://lartedelgelato.com/wp/wp-content/uploads/2016/01/lartedelgelato-3.jpg",
+    		"http://www.gelatogo.net/wp-content/uploads/2016/09/gelato-organico.jpg",
+    		"http://img.aws.livestrongcdn.com/ls-article-image-673/cpi.studiod.com/www_livestrong_com/photos.demandstudios.com/getty/article/103/35/483914165_XS.jpg",
 	];
 	msg.channel.send({
 		embed: {
