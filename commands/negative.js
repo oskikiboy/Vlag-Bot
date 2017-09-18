ef90496ac80d71fe0bd87168dc463aba1a74e03a
Jono <24398002+positiveterminator@users.noreply.github.com>
2017-09-18 16:25:32 +1000

    Update negative.js

--- commands/negative.js
+++ commands/negative.js
@@ -1,1 +1,11 @@
-
+exports.run = async (bot, msg, suffix) => {
+	msg.channel.send({
+		embed: {
+			color: 0x3669FA,
+			description: `O Fuck, @PositiveTerminator#1633 has been exposed *shocker*`,
+			image: {
+				url: `http://weknowmemes.com/wp-content/uploads/2013/12/oh-for-fucks-sake-gif-1.gif`,
+			},
+		},
+	});
+};
