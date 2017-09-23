72d0c615f8c48c76b71de0272fd245bd74b44942
TTtie <tttiett@outlook.com>
2017-09-23 21:24:25 +0200

    my indentation irl

--- Commands/tttie.js
+++ Commands/tttie.js
@@ -9,17 +9,17 @@ module.exports = class TTtie extends Command {
 		return true;
 	}
 
-  requirementsFail({ msg, suffix }) {
+  	requirementsFail({ msg, suffix }) {
 		msg.reply(`ping b1nzy today`);
 	}
 
 	async run({ msg, suffix }) {
 		msg.channel.send({
-      embed: {
-        color: 0x008800,
-        description: `TTtie, also known as the almighty b1nzy pinger and the god. He is a programmer that likes to put this ***beautiful*** command here. All have to hail him and Vlag the God.${suffix.toLowerCase() == "hail" ? `\nYou have hailed TTtie! (and Vlag too)`:""}`
-      }
-    })
+      			embed: {
+        			color: 0x008800,
+        			description: `TTtie, also known as the almighty b1nzy pinger and the god. He is a programmer that likes to put this ***beautiful*** command here. All have to hail him and Vlag the God.${suffix.toLowerCase() == "hail" ? `\nYou have hailed TTtie! (and Vlag too)`:""}`,
+      			},
+    		})
 	}
 };
 
